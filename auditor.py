a3ec9cf6b691a06c9cb43b1f2e1053c2cb1474b9
Jason Phua <jasonphua2002@gmail.com>
2026-09-13 11:53:50 +0800

    Handle invalid input

---
+++ auditor.py
@@ -0,0 +1,19 @@
+# Smart Inventory Auditor
+
+inventory = 0
+quit = False
+quit_or_continue = ""
+
+while (quit==False):
+    stockqty = input("Please enter stock quantity:")
+    if(stockqty.isdigit()):
+        inventory = inventory+int(stockqty)
+    else:
+        print("Stock quantity must be a number.")
+        continue
+    quit_or_continue = input("To quit, enter 'quit'. Otherwise, to continue adding entries, press Enter:")
+    if quit_or_continue == "quit":
+        quit = True
+    elif quit_or_continue != "":
+        quit_or_continue = input("Invalid input. Please enter 'quit' to exit or press Enter to continue.")
+
