971eedcb935f97170c6880b7cf1941610cc21a59
Jason Phua <jasonphua2002@gmail.com>
2026-09-13 12:05:38 +0800

    reporting

--- auditor.py
+++ auditor.py
@@ -2,18 +2,21 @@
 
 inventory = 0
 quit = False
+failedentry = 0
 quit_or_continue = ""
 
 while (quit==False):
     stockqty = input("Please enter stock quantity:")
     if(stockqty.isdigit()):
         if(int(stockqty)<0):
-                print("Stock quantity cannot be negative.")
-                continue
+            print("Stock quantity cannot be negative.")
+            failedentry = failedentry + 1
+            continue
         else:  
             inventory = inventory+int(stockqty)
     else:
         print("Stock quantity must be a number.")
+        failedentry = failedentry + 1
         continue
     if inventory>500:
         print("Inventory limit exceeded.")
@@ -23,4 +26,7 @@ while (quit==False):
         quit = True
     elif quit_or_continue != "":
         quit_or_continue = input("Invalid input. Please enter 'quit' to exit or press Enter to continue.")
+
+print("Total Units Processed: ", inventory)
+print("Total Failed entries: ", failedentry)
 
